c4056bf316fad114feeeb69b8755e97185db976e
Kyle Gundry <Gundr1kt@gmail.com>
2017-04-05 19:08:40 -0500

    Initial Commit

---
+++ app/controllers/forecasts_controller.rb
@@ -0,0 +1,11 @@
+class ForecastsController < ApplicationController
+  def index
+    @city = params[:city] || "detroit"
+    @state = params[:state] || "mi"
+    @forecasts = Unirest.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22#{@city}%2C%20#{@state}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").body
+    @temperature = @forecasts["query"]["results"]["channel"]["item"]["condition"]["temp"]
+    @date = @forecasts["query"]["results"]["channel"]["item"]["condition"]["date"]
+    @days = @forecasts["query"]["results"]["channel"]["item"]["forecast"]
+    render "index.html.erb"
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  get "/forecasts" => "forecasts#index"
+end
